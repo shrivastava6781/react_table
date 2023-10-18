370498a52d4ec179e0931662ef3047134fd79303
Aditya_Shrivastava <94391248+shrivastava6781@users.noreply.github.com>
2023-10-18 15:20:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Table from './Components/Table/Table'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import ExcelFilePage from './Pages/ExcelFilePage'
 
 const App = () => {
   return (
